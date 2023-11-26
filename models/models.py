a625e66cf2860c53724c3ad976e445e5680349d3
ReveekJ <sochnevde09@gmail.com>
2023-11-26 22:18:35 +0500

    add column_json

--- models/models.py
+++ models/models.py
@@ -1,10 +1,15 @@
 from sqlalchemy import MetaData, Table, Column, Integer, Text
 
 metadata = MetaData()
+columns_json = {0: 'id',
+                1: 'role',
+                2: 'sub_info',
+                3: 'lang'}
+
 users = Table('users',
               metadata,
-              Column('id', Integer, primary_key=True),
-              Column('role', Text),
-              Column('sub_info', Text),
-              Column('lang', Text)
+              Column(columns_json[0], Integer, primary_key=True),
+              Column(columns_json[1], Text),
+              Column(columns_json[2], Text),
+              Column(columns_json[3], Text)
               )
