a90e06d887c2922423369d5d04708dc1a2467676
ReveekJ <sochnevde09@gmail.com>
2023-12-06 14:42:10 +0500

    add keyboard

---
+++ tgbot/keyboard.py
@@ -0,0 +1,40 @@
+from aiogram.types import InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from tgbot.text import TEXT
+from sesc_info import SESC_Info
+
+
+def get_choose_role_kb(lang: str) -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(text=TEXT('student', lang), callback_data='student')
+    kb.button(text=TEXT('teacher', lang), callback_data='teacher')
+    # The Illusion of choice
+    kb.button(text=TEXT('parent', lang), callback_data='student')
+
+    kb.adjust(1)
+
+    return kb.as_markup()
+
+
+def get_choose_group_kb() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    groups = SESC_Info.GROUP
+
+    for i in groups.keys():
+        kb.button(text=i, callback_data=groups[i])
+
+    kb.adjust(3)
+
+    return kb.as_markup()
+
+
+def get_choose_teacher_kb() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    groups = SESC_Info.TEACHER
+
+    for i in groups.keys():
+        kb.button(text=i, callback_data=groups[i])
+
+    kb.adjust(2)
+
+    return kb.as_markup()
