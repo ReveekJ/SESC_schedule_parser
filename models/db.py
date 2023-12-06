e2a6b90cc83ae45a2ca535efb591dc0b34b22ad0
ReveekJ <sochnevde09@gmail.com>
2023-12-06 21:56:27 +0500

    solve problem: id must be int, like before

--- models/db.py
+++ models/db.py
@@ -27,6 +27,8 @@ class DB:
         self.session = AsyncSession(self.engine)
 
     async def select_user_by_id(self, _id):
+        _id = self.__convert_to_id_type(_id)
+
         query = select(users).where(users.c.id == _id)
         res = await self.session.execute(query)
         final_result = {}
@@ -39,17 +41,32 @@ class DB:
 
     async def create_user(self, **kwargs):
         # what must be in kwargs u can see in models.py
+        # проверка, что переданы все параметры
+        if kwargs.keys() != columns_json.values():
+            raise ValueError('Не хватает параметров для создания пользователя')
+
+        # преобразование id в тип id, который находтся в бд
+        kwargs['id'] = self.__convert_to_id_type(kwargs['id'])
+
         stmt = insert(users).values(**kwargs)
         await self.session.execute(stmt)
         await self.session.commit()
 
     async def delete_user(self, _id):
+        _id = self.__convert_to_id_type(_id)
+
         stmt = delete(users).where(users.c.id == _id)
         await self.session.execute(stmt)
         await self.session.commit()
 
     async def update_user_info(self, _id, **kwargs):
+        _id = self.__convert_to_id_type(_id)
+
         stmt = update(users).where(users.c.id == _id).values(**kwargs)
 
         await self.session.execute(stmt)
         await self.session.commit()
+
+    @staticmethod
+    def __convert_to_id_type(_id):
+        return str(_id)
