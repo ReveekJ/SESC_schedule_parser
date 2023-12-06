33dc74a48cc08b303386c39fc0fe0690ecbff4b6
ReveekJ <sochnevde09@gmail.com>
2023-12-06 14:50:49 +0500

    fix bug

---
+++ models/model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import MetaData, Table, Column, Text
+
+metadata = MetaData()
+columns_json = {0: 'id',
+                1: 'role',
+                2: 'sub_info',
+                3: 'lang'}
+
+users = Table('users',
+              metadata,
+              Column(columns_json[0], Text, primary_key=True),
+              Column(columns_json[1], Text),
+              Column(columns_json[2], Text),
+              Column(columns_json[3], Text)
+              )
