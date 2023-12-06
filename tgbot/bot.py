e5f7e9f24d8ee8d8547083832c34f81500343a73
ReveekJ <sochnevde09@gmail.com>
2023-12-06 14:41:30 +0500

    add bot.py

---
+++ tgbot/bot.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from handlers import registration
+from config import TOKEN
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_routers(registration.router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
