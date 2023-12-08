798d35787f47c878eff3f7d1ede93a0b5fd0488d
ReveekJ <sochnevde09@gmail.com>
2023-12-08 08:15:05 +0500

    update test db to new interface

--- test/test_db.py
+++ test/test_db.py
@@ -1,37 +1,29 @@
 from unittest import TestCase, main
 from models.db import DB
+from models.database import get_async_session
 
 
 class TestDB(TestCase):
-    def setUp(self):
-        self.session = DB()
-
-    async def test_connect(self):
-        self.assertEqual(await self.session.connect(), None)
+    async def setUp(self):
+        self.session = await get_async_session()
 
     async def test_create_user(self):
-        await self.session.connect()
-        await self.session.create_user(id=1,
-                                       role='group',
-                                       sub_info='34',
-                                       lang='ru')
+        await DB().create_user(self.session,
+                               id=1,
+                               role='group',
+                               sub_info='34',
+                               lang='ru')
         # self.assertEqual(await self.session.select_user_by_id(1), [(1, 'group', '34')])
-        self.assertEqual(isinstance(await self.session.select_user_by_id(1), dict), True)
+        self.assertEqual(isinstance(await DB().select_user_by_id(self.session, 1), dict), True)
 
     async def test_update_user(self):
-        await self.session.connect()
-        await self.session.create_user(id=1,
-                                       role='group',
-                                       sub_info='34',
-                                       lang='ru')
-        await self.session.update_user_info(1, role='teacher', sub_info='AAA')
+        await DB().update_user_info(self.session, 1, role='teacher', sub_info='AAA')
 
-        self.assertEqual(isinstance(await self.session.select_user_by_id(1), dict), True)
+        self.assertEqual(isinstance(await DB().select_user_by_id(self.session, 1), dict), True)
 
     async def test_delete_user(self):
-        await self.session.connect()
-        await self.session.delete_user(1)
-        self.assertEqual(await self.session.select_user_by_id(1), [])
+        await DB().delete_user(self.session, 1)
+        self.assertEqual(await DB().select_user_by_id(self.session, 1), [])
 
 
 if __name__ == '__main__':
