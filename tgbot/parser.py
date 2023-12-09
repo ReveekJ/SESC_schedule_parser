74e5d9d15f377b331078318fa65fc2ef19739e2c
ReveekJ <sochnevde09@gmail.com>
2023-12-09 17:03:23 +0500

    add singleton and bug fix

--- tgbot/parser.py
+++ tgbot/parser.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -7,6 +9,15 @@ import datetime
 
 
 class Parser:
+    __instance = None
+
+    # TODO: доделать singleton
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+        return cls.__instance
+
     def __init__(self, expiration_time_of_schedule: int = 20):
         # expiration_time_of_schedule в минутах
         self.expiration_time_of_schedule = expiration_time_of_schedule
@@ -16,8 +27,9 @@ class Parser:
     def parse(self, first_select: str, second_select: str, third_select: str) -> str:
         path = (__file__[:__file__.rfind('/', 0, __file__.rfind('/'))] + '/' +
                 f'schedules/{first_select}_{second_select}_{third_select}__{datetime.datetime.now()}')
+        file = self.__check_file_in_path(path)
 
-        if not self.__check_file_in_path(path):
+        if file == '':
             selects = {'tx_suncschedule_schedule[type]': first_select,
                        f'tx_suncschedule_schedule[{first_select}]': second_select,
                        'tx_suncschedule_schedule[weekday]': third_select}
@@ -34,17 +46,22 @@ class Parser:
                 print('Поймал ошибку')
                 schedule.screenshot(path + '.png')
 
-        return path + '.png'
+            return path + '.png'
+        else:
+            return path[:path.rfind('/') + 1] + file
 
-    def __check_file_in_path(self, path: str) -> bool:
+    def __check_file_in_path(self, path: str) -> str:
         for files in os.scandir(path[:path.rfind('/')]):
             if files.name[:files.name.find('__')] == path[path.rfind('/') + 1: path.find('__')] and \
-                    (datetime.datetime.now() - datetime.datetime.strptime(path[path.find('__') + 2:],
+                    (datetime.datetime.now() - datetime.datetime.strptime(files.name[files.name.find('__') + 2: files.name.find('.png')],
                                                                           '%Y-%m-%d %H:%M:%S.%f')) \
                     < datetime.timedelta(minutes=self.expiration_time_of_schedule):
-                return True
+                return files.name
 
-        return False
+        return ''
 
     def quit(self):
         self.driver.quit()
+
+
+PARSER = Parser()
