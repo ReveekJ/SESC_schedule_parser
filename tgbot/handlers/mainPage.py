8036e06231760d3c7520be5f89304c69b15c9ba8
ReveekJ <sochnevde09@gmail.com>
2023-12-09 17:02:30 +0500

    bug fix

--- tgbot/handlers/mainPage.py
+++ tgbot/handlers/mainPage.py
@@ -1,45 +1,64 @@
 from aiogram import Router, F
-from aiogram.types import CallbackQuery, FSInputFile, Message
+from aiogram.types import CallbackQuery, FSInputFile
 from tgbot.parser import PARSER
-from tgbot.sesc_info import SESC_Info
 from models.db import DB
-
+from models.database import get_async_session
 import datetime
 
+from tgbot.text import TEXT
+from tgbot.keyboard import get_choose_schedule
 
 
 router = Router()
 
 
-
 @router.callback_query(F.data == 'today')
 async def send_schedule_for_today(callback: CallbackQuery):
-    session = DB()
-    await session.connect()
-    user_data = await session.select_user_by_id(callback.message.from_user.id)
+    session = await get_async_session()
+    user_data = await DB().select_user_by_id(session, callback.from_user.id)
+    lang = callback.from_user.language_code
 
-    today = {i: j for j, i in SESC_Info.WEEKDAY}[(datetime.date.today().weekday()+ 1) % 6]
-    PARSER.parse(user_data['role'], user_data['sub_info'], today)
-    schedule = FSInputFile(f"schedules/{user_data['role']}_{user_data['sub_info']}_{today}.png")
+    await callback.message.delete()
+
+    today = str((datetime.date.today().weekday()) % 6 + 1)
+    file = PARSER.parse(user_data['role'], user_data['sub_info'], today)
+    print(file)
+    schedule = FSInputFile(file)
+
     result = await callback.message.answer_photo(
-        schedule,
-        caption=""
-    )
-    await callback.message.answer(result.photo[-1].file_id)
+        schedule)
+
+    await callback.message.answer(result.photo[-1].file_id,
+                                  disable_notification=True)
+
+    await callback.message.answer(TEXT('main', lang),
+                                  reply_markup=get_choose_schedule(lang),
+                                  disable_notification=True)
+
+
 @router.callback_query(F.data == 'tomorrow')
 async def send_schedule_for_tomorrow(callback: CallbackQuery):
-    session = DB()
-    await session.connect()
-    user_data = await session.select_user_by_id(callback.message.from_user.id)
+    session = await get_async_session()
+    user_data = await DB().select_user_by_id(session, callback.from_user.id)
+    lang = callback.from_user.language_code
 
-    today = {i: j for j, i in SESC_Info.WEEKDAY}[(datetime.date.today().weekday()+ 2) % 6]
-    PARSER.parse(user_data['role'], user_data['sub_info'], today)
-    schedule = FSInputFile(f"schedules/{user_data['role']}_{user_data['sub_info']}_{today}.png")
+    await callback.message.delete()
+
+    today = str((datetime.date.today().weekday()) % 6 + 2)
+    file = PARSER.parse(user_data['role'], user_data['sub_info'], today)
+    schedule = FSInputFile(file)
+
     result = await callback.message.answer_photo(
         schedule,
         caption=""
     )
-    await callback.message.answer(result.photo[-1].file_id)
 
-#@router.callback_query(F.data == 'all')
-#async def send_all_schedule(callback: CallbackQuery):
+    await callback.message.answer(result.photo[-1].file_id,
+                                  disable_notification=True)
+
+    await callback.message.answer(TEXT('main', lang),
+                                  reply_markup=get_choose_schedule(lang),
+                                  disable_notification=True)
+
+# @router.callback_query(F.data == 'all')
+# async def send_all_schedule(callback: CallbackQuery):
