fb4bbc714c77ca0442c01c7a356b356e560d025d
ReveekJ <sochnevde09@gmail.com>
2023-12-15 15:06:27 +0500

    add wekdays text

--- tgbot/text.py
+++ tgbot/text.py
@@ -59,4 +59,22 @@ TEXT = TextMessage(('hello', 'Привет! Это бот, котрый може
                    ('month', ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
                               'Октябрь', 'Ноябрь', 'Декабрь'],
                     ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
-                     'November', 'December']))
+                     'November', 'December']),
+                   ('weekdays', {
+                       1: 'Понедельник',
+                       2: 'Вторник',
+                       3: 'Среда',
+                       4: 'Четверг',
+                       5: 'Пятница',
+                       6: 'Суббота',
+                       7: 'Воскресенье'
+                   },
+                    {
+                        1: 'Monday',
+                        2: 'Tuesday',
+                        3: 'Wednesday',
+                        4: 'Thursday',
+                        5: 'Friday',
+                        6: 'Saturday',
+                        7: 'Sunday'
+                    }))
