61a95954c03ee8dd72271a7641f8df856a3b5e5b
ReveekJ <sochnevde09@gmail.com>
2023-12-19 09:50:31 +0500

    add set up parser

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -2,12 +2,17 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import registration, mainPage, allSchedule
 from config import TOKEN
+from tgbot.parser import PARSER
 
 
 async def main():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
     dp.include_routers(registration.router, allSchedule.router, mainPage.router)
+
+    # set up parser
+    PARSER.parse('group', '1', '1')
+
     print('запуск')
     await dp.start_polling(bot)
 
