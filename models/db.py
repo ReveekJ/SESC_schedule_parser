21a5267b6840294b69a36fbad6b6335920909a52
ReveekJ <sochnevde09@gmail.com>
2024-01-01 22:32:26 +0500

    add select_users_by_role_and_sub_info

--- models/db.py
+++ models/db.py
@@ -1,3 +1,6 @@
+import asyncio
+
+from models.database import get_async_session
 from models.model import users, columns_json
 from sqlalchemy import select, insert, delete, update
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -5,8 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 class DB:
     # Возвращает None если запись не найдется, иначе вернется dict
-    async def select_user_by_id(self, session: AsyncSession, _id):
-
+    async def select_user_by_id(self, session: AsyncSession, _id: int) -> dict | None:
         _id = self.__convert_to_id_type(_id)
 
         query = select(users).where(users.c.id == _id)
@@ -15,9 +17,28 @@ class DB:
 
         try:
             for index, elem in enumerate(res.all()[0]):
+                if index == 0:
+                    elem = self.__convert_from_id_type(elem)
                 final_result[columns_json[index]] = elem
         except IndexError:
+            await session.commit()
             return None
+
+        await session.commit()
+        return final_result
+
+    async def select_users_by_role_and_sub_info(self, session: AsyncSession, role: str, sub_info: str) -> list[dict]:
+        query = select(users).where(users.c.role == role, users.c.sub_info == sub_info)
+
+        res = await session.execute(query)
+        final_result = []
+
+        for i, user in enumerate(res.all()):
+            final_result.append({})
+            for index, elem in enumerate(user):
+                if index == 0:
+                    elem = self.__convert_from_id_type(elem)
+                final_result[i][columns_json[index]] = elem
 
         await session.commit()
         return final_result
@@ -51,14 +72,18 @@ class DB:
         await session.commit()
 
     @staticmethod
-    def __convert_to_id_type(_id):
+    def __convert_to_id_type(_id) -> str:
         return str(_id)
+
+    @staticmethod
+    def __convert_from_id_type(_id) -> int:
+        return int(_id)
 
 # SAMPLE USAGE
 # async def main():
 #     session = await get_async_session()
-#     print(await DB().select_user_by_id(session, 111))
-
-# Run the main function
+#     print(await DB().select_users_by_role_and_sub_info(session, 'group', '34'))
+#
+#
+# # Run the main function
 # asyncio.run(main())
-
