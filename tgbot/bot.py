b85732d111671b86a56cecab3c0fb607b1d91c39
ReveekJ <sochnevde09@gmail.com>
2024-01-01 22:31:32 +0500

    add sending_schedule_changes init

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -2,12 +2,13 @@ import asyncio
 from aiogram import Dispatcher
 from handlers import registration, mainPage, allSchedule, auxiliary
 from tgbot.handlers.auxiliary import bot
+from tgbot.handlers.auxiliary import sending_schedule_changes
 
 
 async def main():
     dp = Dispatcher()
     dp.include_routers(auxiliary.router, registration.router, allSchedule.router, mainPage.router)
-
+    await sending_schedule_changes()
     print('запуск')
     await dp.start_polling(bot)
 
