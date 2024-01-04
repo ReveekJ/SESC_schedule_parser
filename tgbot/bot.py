6ec14f27b90239b5a0b353f990ac0efdaf43692c
ReveekJ <sochnevde09@gmail.com>
2024-01-04 10:54:13 +0500

    fix sending_schedule_changes init

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -3,12 +3,13 @@ from aiogram import Dispatcher
 from handlers import registration, mainPage, allSchedule, auxiliary
 from tgbot.handlers.auxiliary import bot
 from tgbot.handlers.auxiliary import sending_schedule_changes
+from threading import Thread
 
 
 async def main():
     dp = Dispatcher()
     dp.include_routers(auxiliary.router, registration.router, allSchedule.router, mainPage.router)
-    await sending_schedule_changes()
+    Thread(target=sending_schedule_changes).start()
     print('запуск')
     await dp.start_polling(bot)
 
