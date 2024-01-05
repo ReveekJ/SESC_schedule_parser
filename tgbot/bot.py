46f006d8cbfc6d4f683b8b4c486c9d8782af25bc
ReveekJ <sochnevde09@gmail.com>
2024-01-05 18:58:41 +0500

    add relogin router

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import Dispatcher
-from handlers import registration, mainPage, allSchedule, auxiliary
+from handlers import registration, mainPage, allSchedule, auxiliary, relogin
 from tgbot.handlers.auxiliary import bot
 from tgbot.handlers.auxiliary import sending_schedule_changes
 from threading import Thread
@@ -8,8 +8,10 @@ from threading import Thread
 
 async def main():
     dp = Dispatcher()
-    dp.include_routers(auxiliary.router, registration.router, allSchedule.router, mainPage.router)
+    dp.include_routers(auxiliary.router, registration.router, allSchedule.router, mainPage.router, relogin.router)
+
     Thread(target=sending_schedule_changes).start()
+
     print('запуск')
     await dp.start_polling(bot)
 
