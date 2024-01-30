c62524874533710623eb1b34728e367c99614c8f
ReveekJ <sochnevde09@gmail.com>
2024-01-30 10:37:35 +0500

    unlock auditory

--- tgbot/keyboard.py
+++ tgbot/keyboard.py
@@ -74,7 +74,7 @@ def get_choose_type_kb(lang: str) -> InlineKeyboardMarkup:
 
     for i, j in SESC_Info.TYPE.items():
         # ignore all schedule
-        if j in ['all', 'auditory']:
+        if j in ['all']:
             continue
         kb.button(text=i, callback_data='type_' + j)
     add_back_btn(kb, lang)
