55f5b73a489c64c6dc068c6a887c9bb9d5ce9b09
ReveekJ <sochnevde09@gmail.com>
2024-02-13 22:21:16 +0500

    refactor to pep8

--- tgbot/handlers/feedback.py
+++ tgbot/handlers/feedback.py
@@ -1,12 +1,12 @@
 from aiogram import Router, F
 from aiogram.types import Message, FSInputFile
 from typing import Union
-import os
 from aiogram.filters import BaseFilter
 from config import ADMINS
 from tgbot.handlers.auxiliary import bot
 
 router = Router()
+
 
 class ChatTypeFilter(BaseFilter):
     def __init__(self, chat_ignor_type: Union[str, list]):
@@ -18,14 +18,17 @@ class ChatTypeFilter(BaseFilter):
         else:
             return message.chat.type not in self.chat_type
 
+
 @router.message(ChatTypeFilter(chat_ignor_type=['group', 'supergroup']) and F.text)
 async def feedback(message: Message):
     for admin in ADMINS:
         await bot.send_message(admin, message.text,
                                disable_notification=True)
+
+
 @router.message(ChatTypeFilter(chat_ignor_type=['group', 'supergroup']) and F.photo)
 async def feedback_photo(message: Message):
-    path_to_feedback_photo = f"{__file__[:__file__.rfind('/', 0, __file__.rfind('/', 0, __file__.rfind('/')))] + '/temp/' + message.photo[-1].file_id}"
+    path_to_feedback_photo = f"{__file__[:__file__.rfind('/', 0, __file__.rfind('/', 0, __file__.rfind('/')))] + '/temp/' + message.photo[-1].file_id}.png"
     await bot.download(
         message.photo[-1],
         destination=path_to_feedback_photo
@@ -33,4 +36,4 @@ async def feedback_photo(message: Message):
     image_from_pc = FSInputFile(path_to_feedback_photo)
     for admin in ADMINS:
         await bot.send_photo(admin, image_from_pc,
-                               disable_notification=True)
+                             disable_notification=True)
