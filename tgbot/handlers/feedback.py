03c2ace6d6739e505256fecd30718082de23544a
ZalenoToTo <aleksandrvisnev470@gmail.com>
2024-02-15 22:53:42 +0500

    feedback video(unstable)

--- tgbot/handlers/feedback.py
+++ tgbot/handlers/feedback.py
@@ -1,9 +1,10 @@
 from aiogram import Router, F
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message, FSInputFile, ContentType
 from typing import Union
 from aiogram.filters import BaseFilter
 from config import ADMINS
 from tgbot.handlers.auxiliary import bot
+import uuid
 
 router = Router()
 
@@ -26,14 +27,51 @@ async def feedback(message: Message):
                                disable_notification=True)
 
 
-@router.message(ChatTypeFilter(chat_ignor_type=['group', 'supergroup']) and F.photo)
+@router.message(ChatTypeFilter(chat_ignor_type=['group', 'supergroup']) and F.video)
 async def feedback_photo(message: Message):
-    path_to_feedback_photo = f"{__file__[:__file__.rfind('/', 0, __file__.rfind('/', 0, __file__.rfind('/')))] + '/temp/' + message.photo[-1].file_id}.png"
-    await bot.download(
-        message.photo[-1],
-        destination=path_to_feedback_photo
-    )
-    image_from_pc = FSInputFile(path_to_feedback_photo)
-    for admin in ADMINS:
-        await bot.send_photo(admin, image_from_pc,
-                             disable_notification=True)
+    path_to_feedback = f"{__file__[:__file__.rfind('/', 0, __file__.rfind('/', 0, __file__.rfind('/')))] + '/temp/' + str(uuid.uuid4())}"
+    if message.content_type == ContentType.PHOTO:
+        await bot.download(
+            message.photo[-1],
+            destination=path_to_feedback + '.png')
+        image_from_pc = FSInputFile(path_to_feedback)
+
+        for admin in ADMINS:
+            await bot.send_photo(admin, image_from_pc,
+                                 disable_notification=True)
+
+    elif message.content_type == ContentType.VIDEO:
+        # await bot.download(
+        #     message.video,
+        #     destination=path_to_feedback + '.mp4')
+        video = message.video.file_id
+
+        for admin in ADMINS:
+            await bot.send_video(admin, video,
+                                 disable_notification=True)
+
+    elif message.content_type == ContentType.VOICE:
+        # await bot.download(
+        #     message.audio[-1],
+        #     destination=path_to_feedback + '.mp3')
+        # image_from_pc = FSInputFile(path_to_feedback)
+        image_from_pc = message.voice.file_id
+        for admin in ADMINS:
+            await bot.send_voice(admin, image_from_pc,
+                                 disable_notification=True)
+    elif message.content_type == ContentType.VIDEO_NOTE:
+        await bot.download(
+            message.video_note[-1],
+            destination=path_to_feedback + '.mp4')
+        image_from_pc = FSInputFile(path_to_feedback)
+        for admin in ADMINS:
+            await bot.send_video(admin, image_from_pc,
+                                 disable_notification=True)
+    elif message.content_type == ContentType.ANIMATION:
+        await bot.download(
+            message.animation[-1],
+            destination=path_to_feedback + '.mp4')
+        image_from_pc = FSInputFile(path_to_feedback)
+        for admin in ADMINS:
+            await bot.send_video(admin, image_from_pc,
+                                 disable_notification=True)
