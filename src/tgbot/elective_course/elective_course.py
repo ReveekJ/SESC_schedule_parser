079d018f8e43ce9edbee0b64837d90c2d0394c52
ReveekJ <sochnevde09@gmail.com>
2024-05-30 20:08:07 +0500

    add update and cancel methods

--- src/tgbot/elective_course/elective_course.py
+++ src/tgbot/elective_course/elective_course.py
@@ -1,6 +1,9 @@
-from sqlalchemy import select, delete
+import asyncio
+
+from sqlalchemy import select, delete, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.database import get_async_session
 from src.tgbot.elective_course.models import ElectiveCourseModel
 from src.tgbot.elective_course.schemas import ElectiveCourse
 
@@ -17,14 +20,31 @@ class ElectiveCourseDB:
         await session.execute(stmt)
         await session.commit()
 
-    # TODO: fix it, now it does not work
-    # @staticmethod
-    # async def update_course(session: AsyncSession, course: ElectiveCourse):
-    #     course.timetable = json.dumps(course.timetable)
-    #     stmt = update(ElectiveCourseModel).where(ElectiveCourseModel.name == course.name).values(dict(course))
-    #
-    #     await session.execute(stmt)
-    #     await session.commit()
+    @staticmethod
+    async def update_course(session: AsyncSession, course_name: str, new_course: ElectiveCourse):
+        course = new_course.model_dump(exclude={'id'})  # Exclude 'id' field
+
+        stmt = (update(ElectiveCourseModel)
+                .where(ElectiveCourseModel.subject == course_name)
+                .values(**course))
+        # query = select(ElectiveCourseModel).where(ElectiveCourseModel.subject == course_name)
+
+        await session.execute(stmt)
+        await session.commit()
+
+    @staticmethod
+    async def cancel_course(session: AsyncSession, course_name: str):
+        old_course = (await ElectiveCourseDB.get_courses_by_subject(session, course_name))[0]
+        new_course = ElectiveCourse(subject=old_course.subject,
+                                    teacher_name=old_course.teacher_name,
+                                    pulpit=old_course.pulpit,
+                                    weekday=old_course.weekday,
+                                    time_from=old_course.time_from,
+                                    time_to=old_course.time_to,
+                                    auditory=old_course.auditory,
+                                    is_cancelled=True)
+
+        await ElectiveCourseDB.update_course(session, course_name, new_course)
 
     @classmethod
     async def get_courses_by_subject(cls, session: AsyncSession, subject: str) -> list[ElectiveCourse]:
