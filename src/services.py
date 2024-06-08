dc9e35a55f24edca7183bc8550c5aa33a3c7877d
ReveekJ <sochnevde09@gmail.com>
2024-06-08 16:45:47 +0500

    include lycreg router

--- src/services.py
+++ src/services.py
@@ -7,10 +7,12 @@ if sys.path[0] != '/home/user/PycharmProjects/SESC_parser':
 from fastapi import FastAPI
 
 from crypto_service import crypto
+from lycreg_request_service import lycreg
+
 
 app = FastAPI(
     title='SESC_schedule_services'
 )
 
 app.include_router(crypto.router)
-
+app.include_router(lycreg.router)
